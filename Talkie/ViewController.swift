70ad96867f0935c48acad591119ad27e5e65856d
Ryuji Ganaha <ryujignh@gmail.com>
2018-04-26 00:53:58 +0900

    Adding icons.

--- Talkie/ViewController.swift
+++ Talkie/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var painScaleButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initializeImageButtons()
+    }
+    
+    // Initializing UIImageView as touchable buttons
+    func initializeImageButtons() {
+        painScaleButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.PainScaleButtonTapped(_:))))
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func PainScaleButtonTapped(_ sender: UITapGestureRecognizer) {
+        print("タップ")
+        self?.performSegue(withIdentifier: "move to detail", sender: image)
 
+    }
+    
+    
 
+    
+    
 }
 
